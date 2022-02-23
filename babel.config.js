b9f21ad639a52c3143704f2a1533b1b8ab7b9af9
andy-pat <andrewjp1992@gmail.com>
2022-02-23 12:54:37 +0000

    fixed

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  // presets: ["@babel/preset-env"],
-  presets: ["@vue/cli-plugin-babel/preset"],
-};
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
+}
--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,63 @@
 <template>
   <div class="container">
+    <Letters></Letters>
     <div class="buttons">
-      <button @click="toggleModal">gigs</button>
+      <div class="gig-listings">
+        <h1>upcoming gigs:</h1>
+        <ul>
+          <a href="https://mif.co.uk/whats-on/festival-square-8-july/">
+            <li>MIF 8th July 18.30</li>
+          </a>
+        </ul>
+      </div>
+      <!-- <button>Sound </button> -->
       <a href="https://www.youtube.com/watch?v=szddj-LsmW8" target="_blank">
-        <button><i class="fab fa-youtube"></i></button>
+        <button>Sound & Vision</button>
       </a>
       <a
         href="https://www.instagram.com/nonunonu_/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <button><i class="fab fa-instagram"></i></button>
+        <button>Vision</button>
       </a>
     </div>
   </div>
-  <Letters />
-  <Gigs v-if="displayGigs" :gigs="gigs" @closeModal="toggleModal" />
 </template>
 
 <script>
 import Letters from "./components/Letters.vue";
-import Gigs from "./components/Gigs.vue";
+
 export default {
   name: "App",
   components: {
     Letters,
-    Gigs,
-  },
-  data() {
-    return {
-      displayGigs: false,
-      gigs: [
-        {
-          venue: "Temple Of Art and Music",
-          date: "22/02/22",
-          link: "https://www.facebook.com/events/1197110244453739?ref=newsfeed",
-        },
-        {
-          venue: "Fuel",
-          link: "https://www.facebook.com/fuelcafebar/",
-          date: "13/03/22",
-        },
-        {
-          venue: "Matt & Phreds",
-          date: "23/03/22",
-        },
-        {
-          venue: "Revolver Liverpool",
-          link: "https://revolverliverpool.co.uk/cavern-quarter-jazz/",
-          date: "25/04/22",
-        },
-        {
-          venue: "Manchester Jazz Festival",
-          date: "24/05/22",
-          link: "https://manchesterjazz.com/",
-        },
-        {
-          venue: "Eskfest",
-          date: "08/07/22",
-        },
-      ],
-    };
-  },
-  methods: {
-    toggleModal() {
-      this.displayGigs = !this.displayGigs;
-    },
   },
 };
 </script>
 
-<style lang="scss">
+<style>
 body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  padding: 0;
-  background-color: #11187a85;
   margin: 0;
-  height: 100%;
-  overflow: hidden;
+  padding: 0;
+  background-color: #3bd6e685;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  overflow-x: hidden;
 }
 
 #app {
   width: 100%;
-  height: 100%;
-  overflow: hidden;
-  position: static;
 }
+
+/* .container {
+} */
 
 .gig-listings {
   position: absolute;
@@ -97,19 +65,25 @@ body {
 }
 
 .buttons {
-  margin-top: 2rem;
   display: flex;
-  padding: 0 3rem;
+  align-items: center;
+  align-self: center;
   justify-content: space-evenly;
-  button {
-    padding: 0.8rem;
-    border: dotted 2px darkgreen;
-    border-radius: 1rem;
-    background-color: #c1aed2;
-    font-family: Courier;
-  }
+  width: 100%;
+  margin-top: 2rem;
+}
+.button {
+  width: 80px;
+  height: 4rem;
+  border: dotted 2px darkgreen;
+  border-radius: 1rem;
+  background-color: #c1aed2;
+  font-family: Courier;
 }
 
 @media only screen and (max-width: 500px) {
+  #app {
+    width: fit-content;
+  }
 }
 </style>
